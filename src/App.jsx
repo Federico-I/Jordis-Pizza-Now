f921792c0a5339cf47317c851021b633a724556b
Federico-I <threxos.48@gmail.com>
2024-09-23 23:59:44 +0100

    lel

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+createBrowserRouter([
+  {
+    path:"/",
+    element: <Home />,
+  },
+  {
+    path:"/menu",
+    element: <Menu />,
+  }
+])
 
 function App() {
 
