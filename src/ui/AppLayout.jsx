1d43cff26d246b69fb74d3d5e593b30a256f02bb
Federico-I <threxos.48@gmail.com>
2024-09-24 22:34:21 +0100

    applayout-prox-dataFetching

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,7 +2,9 @@
 import React from 'react';
 import Header from './Header';
 import CartOverview from '../features/cart/CartOverview';
+import { Outlet } from 'react-router-dom';
 
+// will implement loaders and menu fetched from apiRestaurant
 
 function AppLayout() {
   return (
@@ -10,7 +12,7 @@ function AppLayout() {
       <Header />
 
       <main>
-        <h1>Example</h1>
+        <Outlet />
       </main>
 
       <CartOverview/>
