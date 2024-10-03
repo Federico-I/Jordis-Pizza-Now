5e9dadff9a145dd9f8bcbd31c243841176d3d1c5
Federico-I <threxos.48@gmail.com>
2024-10-03 22:52:18 +0100

    renderFetched-Data-strategy

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,8 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { getMenu } from "../../services/apiRestaurant";
+import { useLoaderData } from "react-router-dom";
 
 function Menu() {
+
+  ////// Render as you fetch strategy ///////
+  const menu = useLoaderData();
+
   return <h1>Menu</h1>;
 }
 
-export default Menu;
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu ;
