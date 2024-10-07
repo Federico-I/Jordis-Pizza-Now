fd03e1ff9e78d7e2e2c3adb89753b9d69b9d9e00
Federico-I <threxos.48@gmail.com>
2024-10-07 22:18:40 +0100

    errorHandling-useRouteError

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
@@ -19,6 +20,7 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path:"/", element: <Home />},
       { path:"/menu", element: <Menu />, loader: menuLoader},
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { formatCurrency } from "../../utils/helpers";
 
+
 function MenuItem({ pizza }) {
   const [ id, name, unitPrice, ingredients, soldOut, imageUrl ] = pizza;
 
