0de817450a005fc8d1763e41d9458a6d0a4fe3fa
Federico-I <threxos.48@gmail.com>
2024-10-14 22:02:36 +0100

    phoneNumber-errorHandling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { useState } from "react";
-import { Form, redirect, useNavigate, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigate, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 
@@ -40,6 +40,8 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
+  const formErrors = useActionData();
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -58,6 +60,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
