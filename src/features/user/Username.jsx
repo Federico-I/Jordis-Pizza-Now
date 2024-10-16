79a0dc6982f60f8a5fc13bb2af33e86b40367ddc
Federico-I <threxos.48@gmail.com>
2024-10-16 20:55:28 +0100

    username-file-styling

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function Username() {
+  return (
+    <div className='text-sm font-semibold'>Fede</div>
+  )
+}
+
+export default Username;
