f5228de5c604cb41f651b4a5320bc63e103aea5f
Federico-I <threxos.48@gmail.com>
2024-10-23 23:50:07 +0100

    createOrder-styling-AddressComp

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -66,7 +66,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input className="rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 w-full" type="text" name="address" required />
           </div>
         </div>
 
