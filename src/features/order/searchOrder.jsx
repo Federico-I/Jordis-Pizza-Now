30059d3b9a3c894700795c8107a9218abd4fa994
Federico-I <threxos.48@gmail.com>
2024-10-23 23:35:26 +0100

    searchOrder-styling

--- src/features/order/searchOrder.jsx
+++ src/features/order/searchOrder.jsx
@@ -16,8 +16,8 @@ function SearchOrder() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder='Search for #' value={query} onChange={(e) => setQuery(e.target.value)
-      }/>
+      <input placeholder='Search for #' value={query} onChange={(e) => setQuery(e.target.value)} className='rounded-full px-4 py-2 text-sm placeholder:text-stone-400 w-28 sm:w-64 focus:w-72 sm:w-64 transition-all duration-300'
+      />
     </form>
   )
 }
