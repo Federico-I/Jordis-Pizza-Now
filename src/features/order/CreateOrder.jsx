82cfa1ce4b7cf61663fd5d8b389e53da42993478
Federico-I <threxos.48@gmail.com>
2024-10-25 19:50:14 +0100

    mediaQueries-applied-Button

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -84,7 +84,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
-          <Button className="inline-block rounded-full bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed" disabled={isSubmitting ? "Placing order..." : "Order now!"} >
+          <Button className="inline-block rounded-full bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed " disabled={isSubmitting ? "Placing order..." : "Order now!"} >
             Order now
           </Button>
         </div>
