076ee1be89a52c6bffb39930397d78f4a76e226e
Federico-I <threxos.48@gmail.com>
2024-10-25 19:33:43 +0100

    reusing-stylinClasses-NewComps

---
+++ src/ui/Button.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types,
+function Button({ children, disabled }) {
+  return (
+    <button className="inline-block rounded-full bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed">
+      { children }
+    </button>
+  )
+}
+
+export default Button
