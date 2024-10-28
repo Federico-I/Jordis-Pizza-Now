18b74815da982e9048037d036b438939126b0698
Federico-I <threxos.48@gmail.com>
2024-10-28 21:51:58 +0000

    cartButton-customClasses

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,15 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types,
-function Button({ children, disabled }) {
+function Button({ children, disabled, to}) {
 
   const className = "inline-block rounded-full bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 sm:py-4" ;
 
-  if (to) return <Link className={className}>{children}</Link>; 
+  if (to) 
+  return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    ); 
 
   return (
-    <button disabled={disabled} className={className} to={}>
+    <button disabled={disabled} className={className} to={to}>
       { children }
     </button>
   )
