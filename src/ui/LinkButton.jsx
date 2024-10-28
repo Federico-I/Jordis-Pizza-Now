d0b05379c58a6bd100573515c30489ce94167fbc
Federico-I <threxos.48@gmail.com>
2024-10-28 19:16:27 +0000

    Error-Button-newFeature

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,10 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+  const className = 'text-sm text-blue-500 hover:text-blue-900 hover:underline';
+
+  if (to === "-1")
+    return <button onClick={() => navigate(-1)}>{ children }</button>
+
   return (
-    <Link to={to} className='text-sm text-blue-500 hover:text-blue-900 hover:underline'>{children}</Link>
+    <Link to={to} className={className}>
+      {children}
+    </Link>
   );
 }
 
