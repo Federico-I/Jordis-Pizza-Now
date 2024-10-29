03c10831dd07b7a0c540bf70f1be46ad8c2691d6
Federico-I <threxos.48@gmail.com>
2024-10-29 01:03:37 +0000

    tailwindConfig-fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      pizza: "Roboto Mono, monospace",
+    },
+
     extend: {},
   },
   plugins: [],
