cb7dee3080ef5749f056bbb6790c73457fee2f37
Federico-I <threxos.48@gmail.com>
2024-10-31 05:48:19 +0000

    adding-conditional-styling

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -7,13 +7,13 @@ function MenuItem({ pizza }) {
   const [ id, name, unitPrice, ingredients, soldOut, imageUrl ] = pizza;
 
   return (
-    <li className="flex gap-4">
-      <img src={imageUrl} alt={name} className="h-24"/>
-      <div>
+    <li className="flex gap-4 py-2">
+      <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`} />
+      <div className="flex flex-col">
         <p>{name}</p>
         <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        <div className="mt-auto">
+          {!soldOut ? ( <p className="text-sm uppercase font-medium text-stone-500">{formatCurrency(unitPrice)}</p> ) : ( <p>Sold out</p> )}
         </div>
       </div>
     </li>
