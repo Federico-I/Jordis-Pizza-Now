01a98e56589d39612d93735807a9226e12386981
Federico-I <threxos.48@gmail.com>
2024-11-01 23:46:27 +0000

    styling-orderbutton

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -24,7 +24,7 @@ function CreateUser() {
 
       {username !== '' && (
         <div>
-          <Button>Start ordering</Button>
+          <Button typr="primary" >Start ordering</Button>
         </div>
       )}
     </form>
