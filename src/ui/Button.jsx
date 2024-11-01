b491c9a981f65b7d5b96984ac65eea06a1350587
Federico-I <threxos.48@gmail.com>
2024-11-01 23:57:59 +0000

    styling-button-primary-small-classes

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,23 +5,22 @@ import { Link } from 'react-router-dom';
 // eslint-disable-next-line react/prop-types,
 function Button({ children, disabled, to, type}) {
 
-  const className = "inline-block rounded-full bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 sm:py-4" ;
-
-  const base = "inline-block rounded-full bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 sm:py-4";
+  const base = "inline-block rounded-full bg-yellow-400 uppercase font-semibold text-stone-800 tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const style = {
-    primary: base + "px-4 py-3 sm:px-6 sm:py-4",
+    primary: base + "px-4 py-3 md:px-6 md:py-4",
+    small: base + "px-4 py-2 md:px-5 md:py-2.5"
   }
 
   if (to) 
   return (
-      <Link to={to} className={className}>
+      <Link to={to} className={style[type]}>
         {children}
       </Link>
     ); 
 
   return (
-    <button disabled={disabled} className={className} to={to}>
+    <button disabled={disabled} className={style[type]} to={to}>
       { children }
     </button>
   )
