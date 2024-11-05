421bcee68002c52b1e9ba02384e56e734a4551c1
Federico-I <threxos.48@gmail.com>
2024-11-05 01:57:33 +0000

    addingMediaQueries-toButton-cart-CartItem

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -10,6 +10,7 @@ function Button({ children, disabled, to, type}) {
   const style = {
     primary: base + "px-4 py-3 md:px-6 md:py-4",
     small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary: "inline-block rounded-full border-2 border-stone-300 uppercase font-semibold text-stone-400 tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   }
 
   if (to) 
