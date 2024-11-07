827c3620483ec71b3090da18d79162f2a67831e2
Federico-I <threxos.48@gmail.com>
2024-11-07 23:29:10 +0000

    createOrder-ErrorMessage-fixed-flexApplied

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -58,15 +58,15 @@ function CreateOrder() {
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Phone number</label>
-          <div>
+          <div className="grow">
             <input className="input" type="tel" name="phone" required />
+            {formErrors?.phone && <p>{formErrors.phone}</p>}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
-          <div>
+          <div className="grow">
             <input className="input" type="text" name="address" required />
           </div>
         </div>
