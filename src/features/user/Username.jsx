3874ace528ada89c28ddeea5cd9e14f04e962e39
Federico-I <threxos.48@gmail.com>
2024-11-22 20:56:57 +0000

    passing-state-useSelect-Username

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Username() {
+  useSelector( state => state.user.username )
+  
   return (
     <div className='hidden text-sm font-semibold md:block'>Fede</div>
   )
