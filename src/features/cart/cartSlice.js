f366b9e6e8fb2e3ce3264f89dbe3064c3bc7c404
Federico-I <threxos.48@gmail.com>
2024-11-27 19:58:23 +0000

    addingReducer-toCartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { createSlice } from "@reduxjs/toolkit";
 import reducer from "../user/userSlice";
 
 const initialState = {
-  cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitprice: 16,
+      totalPrice: 32,
+    },
+  ],
 };
 
 const cartSlice = {
   name: "Cart",
   initialState,
+  reducer: {
+    addItem(state, action) {},
+    deleteItem(state, action) {},
+    increaseitemQuantity(state, action) {},
+    decreaseItemQuantity(state, action) {},
+    clearCart(state, action) {},
+  },
 };
