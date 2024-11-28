37bd4f3fa1c1b5750682a2aac85eeb7635742b09
Federico-I <threxos.48@gmail.com>
2024-11-28 23:48:56 +0000

    adding-decreaseItemQuantity-andClearCart-reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -11,7 +11,7 @@ const initialState = {
       pizzaId: 12,
       name: "Mediterranean",
       quantity: 2,
-      unitprice: 16,
+      unitPrice: 16,
       totalPrice: 32,
     },
     {
@@ -36,12 +36,22 @@ const cartSlice = {
       // payload = pizzaId
       state.cart = state.cart.filter((item) => item.pizzaId !== action.payload)
     },
-    increaseitemQuantity(state, action) {
+    increaseItemQuantity(state, action) {
       const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
     },
-    decreaseItemQuantity(state, action) {},
-    clearCart(state, action) {},
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
   },
 };
 
-export default {initialState, cartSlice};
+export const {addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart} = cartSlice.action;
