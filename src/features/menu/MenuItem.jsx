0320def76daa13adfb5c1cd9470e7eb7cab2b454
Federico-I <threxos.48@gmail.com>
2024-11-29 07:53:12 +0000

    conditionalButton-MenuItem

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -7,6 +7,8 @@ import Button from "../../ui/Button";
 function MenuItem({ pizza }) {
   const [ id, name, unitPrice, ingredients, soldOut, imageUrl ] = pizza;
 
+  function handleAddToCart() {};
+
   return (
     <li className="flex gap-4 py-2">
       <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`} />
@@ -16,7 +18,7 @@ function MenuItem({ pizza }) {
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? ( <p className="text-sm">{formatCurrency(unitPrice)}</p> ) : ( <p className="text-sm uppercase font-medium text-stone-500" >Sold out</p> )}
 
-          <Button type="small"> Add to Cart</Button>
+          {!soldOut && <Button type="small" onClick={handleAddToCart}> Add to Cart</Button>}
         </div>
       </div>
     </li>
