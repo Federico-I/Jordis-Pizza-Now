10297f134f370f7a9846f0ce4e2742778276b041
Federico-I <threxos.48@gmail.com>
2024-12-04 21:31:42 +0000

    totalCarPrice-feature-implemented

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,16 +2,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getTotalCartPrice } from "./cartSlice";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
 function CartOverview() {
-  const totalCartQuantity = useSelector(getTotalCartPrice);
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
 
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 sm:px-6 text-sm md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartPrice}</span>
       </p>
       <Link href="/cart">Open cart &rarr;</Link>
     </div>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -51,7 +51,7 @@ export const {addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, c
 
 export const getTotalCartQuantity = (state) => state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
-export const getTotalCartPrice = (state) => state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+export const getTotalCartPrice = (state) => state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
 
 export default cartSlice.reducer;
