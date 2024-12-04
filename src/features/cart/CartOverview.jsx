558ebac0bbc37fdd9ffa76ad6a357dd13a3811d5
Federico-I <threxos.48@gmail.com>
2024-12-04 21:13:23 +0000

    add-getTotalCartPrice

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,9 +2,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartPrice } from "./cartSlice";
 
 function CartOverview() {
-  const totalCartQuantity = useSelector();
+  const totalCartQuantity = useSelector(getTotalCartPrice);
 
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 sm:px-6 text-sm md:text-base">
