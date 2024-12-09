b2051518bcfcb1908bb0a9397b8f51743334f36c
Federico-I <threxos.48@gmail.com>
2024-12-09 00:28:08 +0000

    fixed-typos-settingUp-Delete

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -2,10 +2,10 @@ import { LinkButton } from 'react-router-dom';
 
 function EmptyCart() {
   return (
-    <div className='px-4py-3'>
-      <LinkButton to="/menu" >&larr; Back to menu</LinkButton>
+    <div className='px-4 py-3'>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <p className='font-semibold'>Your cart is still empty. Start adding some pizzas!</p>
+      <p className='mt-7 font-semibold'>Your cart is still empty. Start adding some pizzas!</p>
     </div>
   );
 }
