4e8a4c7f331c6eb77eca213d77b90833e1cd42ca
Federico-I <threxos.48@gmail.com>
2024-12-09 00:40:35 +0000

    adding-dispatch

--- src/features/cart/DeleteItem.jsx
+++ src/features/cart/DeleteItem.jsx
@@ -1,10 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { deleteItem } from './cartSlice';
 
-function DeleteItem() {
+function DeleteItem({ pizzaId }) {
+  const dispatch = useDispatch();
+
   return (
-    <Button type="small">Delete</Button>
+    <Button type="small" on onClick={() => dispatchEvent(deleteItem(pizzaId))}>Delete</Button>
   );
 }
 
