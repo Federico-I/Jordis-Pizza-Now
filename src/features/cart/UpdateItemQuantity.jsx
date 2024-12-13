49d2e467ad07900d053b5a7851a1a60973a89055
Federico-I <threxos.48@gmail.com>
2024-12-13 21:03:44 +0000

    styling-updateItem

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,10 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import Button from '../../ui/Button';
 
-function UpdateItemQuantity() {
+function UpdateItemQuantity({ pizzaId }) {
   return (
-    <div>UpdateItemQuantity</div>
-  )
+    <div>
+      <Button type="round">-</Button>
+      <Button type="round">+</Button>
+    </div>
+  );
 }
 
 export default UpdateItemQuantity;
