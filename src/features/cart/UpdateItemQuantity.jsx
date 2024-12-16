ad773b788c6019915314593b69f5903acfc160e7
Federico-I <threxos.48@gmail.com>
2024-12-16 22:49:34 +0000

    applying-useDispatch-updateItemComp

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,11 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
 
 function UpdateItemQuantity({ pizzaId }) {
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <Button type="round">-</Button>
+    <div className='flex gap-1 items-center md:gap-3 '>
+      <Button type="round" onClick={() => dispatch()}>-</Button>
       <Button type="round">+</Button>
     </div>
   );
