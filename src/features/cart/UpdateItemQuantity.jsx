b3d4af460be726e146954a86a0d0afe1ef274d42
Federico-I <threxos.48@gmail.com>
2024-12-16 23:00:23 +0000

    applying-increasFunctionality-UpdatequantityComp

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -2,14 +2,15 @@
 import React from 'react';
 import Button from '../../ui/Button';
 import { useDispatch } from 'react-redux';
+import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 
 function UpdateItemQuantity({ pizzaId }) {
   const dispatch = useDispatch();
 
   return (
     <div className='flex gap-1 items-center md:gap-3 '>
-      <Button type="round" onClick={() => dispatch()}>-</Button>
-      <Button type="round">+</Button>
+      <Button type="round" onClick={() => dispatch(decreaseItemQuantity(pizzaId))}>-</Button>
+      <Button type="round" onClick={() => dispatch(increaseItemQuantity(pizzaId))}>+</Button>
     </div>
   );
 }
