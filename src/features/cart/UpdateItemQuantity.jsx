1d66b0c003b3dd7c9a706296e18a8a0eed949753
Federico-I <threxos.48@gmail.com>
2024-12-17 23:16:53 +0000

    settingup-styleToUdateQuantity-Button

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -10,7 +10,7 @@ function UpdateItemQuantity({ pizzaId, currentQuantity }) {
   return (
     <div className='flex gap-1 items-center md:gap-3 '>
       <Button type="round" onClick={() => dispatch(decreaseItemQuantity(pizzaId))}>-</Button>
-      <span>{currentQuantity}</span>
+      <span className='text-sm font-medium'>{currentQuantity}</span>
       <Button type="round" onClick={() => dispatch(increaseItemQuantity(pizzaId))}>+</Button>
     </div>
   );
