ec5d38cfd9b44b4523116f8c17171beab4d1daf5
Federico-I <threxos.48@gmail.com>
2024-12-18 22:43:37 +0000

    setting-UpdateComp-MenuItem

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -9,7 +9,7 @@ import { useSelector } from "react-redux";
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
-  const currentQuantity = useSelector(getCurrentQuantityById([pizzaId]));
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
