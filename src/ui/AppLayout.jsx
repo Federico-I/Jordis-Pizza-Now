dd0756ddac3e5459a8543fd344e6a5b6032f6c05
Federico-I <threxos.48@gmail.com>
2025-01-21 17:14:15 +0000

    appLayout-styling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -12,7 +12,7 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return(
-    <div className='layout'>
+    <div className='h-128 grid bg-red'>
       {isLoading && <Loader />}
 
       <Header />
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -8,8 +8,8 @@ function Home() {
   const username = useSelector((state) => state.user.username);
 
   return (
-    <div className="my-10 sm:my-16 text-center">
-      <h1 className="mb-8 text-xl font-semibold">
+    <div className="my-10 px-4 sm:my-16 text-center">
+      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
