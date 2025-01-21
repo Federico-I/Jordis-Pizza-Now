649d879b17d09c753d10a2d757fc2c715ad60d25
Federico-I <threxos.48@gmail.com>
2025-01-21 17:17:31 +0000

    main-appLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -12,12 +12,12 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return(
-    <div className='h-128 grid bg-red'>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
 
-      <main>
+      <main className="overflow-scroll">
         <Outlet /> 
       </main>
 
