785a5968453841759432f210280fce2f5bf433b8
Federico-I <threxos.48@gmail.com>
2025-01-23 23:43:21 +0000

    styling-Input-CreateOrder

--- src/features/order/searchOrder.jsx
+++ src/features/order/searchOrder.jsx
@@ -17,7 +17,7 @@ function SearchOrder() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder='Search order #' value={query} onChange={(e) => setQuery(e.target.value)} className='w-28 rounded-full px-4 bg-yellow-100 py-2 text-sm placeholder:text-stone-400 sm:w-64 focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500'/>
+      <input placeholder='Search order #' value={query} onChange={(e) => setQuery(e.target.value)} className='w-28 rounded-full px-4 bg-yellow-100 py-2 text-sm placeholder:text-stone-400 sm:w-64 focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50'/>
     </form>
   )
 }
