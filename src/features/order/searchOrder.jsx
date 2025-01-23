b8ad7a837050fd8245ac49a4614aaf82c7b07760
Federico-I <threxos.48@gmail.com>
2025-01-23 23:15:47 +0000

    styling-searchOrder

--- src/features/order/searchOrder.jsx
+++ src/features/order/searchOrder.jsx
@@ -17,7 +17,7 @@ function SearchOrder() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder='Search order #' value={query} onChange={(e) => setQuery(e.target.value)}/>
+      <input placeholder='Search order #' value={query} onChange={(e) => setQuery(e.target.value)} className='rounded-full px-4 py-2 text-sm placeholder:text-stone-400'/>
     </form>
   )
 }
