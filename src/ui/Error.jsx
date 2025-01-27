c59be05ee555ba695f83a2e2e5397b1dba74da56
Federico-I <threxos.48@gmail.com>
2025-01-27 23:01:43 +0000

    setting-LinkButton-conditional-className

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -4,7 +4,6 @@ import { useNavigate, useRouteError } from 'react-router-dom';
 import LinkButton from './LinkButton';
 
 function Error() {
-  const navigate = useNavigate();
   const error = useRouteError();
 
   return (
