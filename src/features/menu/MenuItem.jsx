f16cf0f375a857c1754f4123718688e89a77bcbe
Federico-I <threxos.48@gmail.com>
2025-01-29 21:35:53 +0000

    stylingName-ingredients

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -15,8 +15,8 @@ function MenuItem({ pizza }) {
     <li className="flex gap-4 py-2">
       <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}/>
       <div className="flex flex-col">
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">{ingredients.join(', ')}</p>
         <div className="mt-auto">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm font-medium uppercase text-stone-500">Sold out</p>}
         </div>
