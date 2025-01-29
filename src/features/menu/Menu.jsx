5625d9ec12b6e78a3ff8edeeee1ef077cb25e82b
Federico-I <threxos.48@gmail.com>
2025-01-29 21:23:07 +0000

    menuStyle-update

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -12,7 +12,7 @@ function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul className="divide-y divide-stone-200">
+    <ul className="space-y-2 divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
