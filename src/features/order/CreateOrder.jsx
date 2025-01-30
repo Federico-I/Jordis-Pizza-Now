fd291e055065201c141d3b515847176d454927f5
Federico-I <threxos.48@gmail.com>
2025-01-30 06:21:17 +0000

    adding-presetStyling-toComps

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -89,7 +89,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting}>{isSubmitting ? "Placing order..." : "Order now"}</Button>
+          <Button disabled={isSubmitting} type="primary">{isSubmitting ? "Placing order..." : "Order now"}</Button>
         </div>
       </Form>
     </div>
