206503f067c619319233c822a25849aa9105fa00
Federico-I <threxos.48@gmail.com>
2025-02-07 03:37:49 +0000

    clearingCart-code

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -56,33 +56,3 @@ function Cart() {
 }
 
 export default Cart;
-
-/*
-function Cart() {
-
-  const username = useSelector((state) => state.user.username);
-  const cart = useSelector(getCart);
-  const dispatch = useDispatch();
-
-  if(!cart.lenght) return <EmptyCart/>;
-
-  return (
-    <div className='px-4 py-3'>
-      <LinkButton to="/menu" >&larr; Back to menu</LinkButton>
-
-      <h2 className='mt-7 text-xl font-semibold'>Your cart, {username}</h2>
-      <ul className='divide-y divide-stone-200 border-b mt-3'>
-        {cart.map((item) => <CartItem item={item} key={item.pizzaId}/>)}
-      </ul>
-
-      <div className='mt-6 sace-x-2'>
-        <Button to="/order/new" className="primary">Order pizzas</Button>
-        <Button type="secondary" onClick={() => dispatch(clearCart())}></Button>
-        <button>Clear cart</button>
-      </div>
-    </div>
-  );
-}
-
-export default Cart;
-*/
