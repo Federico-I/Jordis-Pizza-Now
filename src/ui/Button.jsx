4de6841e6e7b70022ba3386b744227aa321f05c0
Federico-I <threxos.48@gmail.com>
2025-02-10 23:44:32 +0000

    addingfocusStyling-toButton

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -9,7 +9,7 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " py-3 px-4 md:px-6 sm:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
-    secondary: "uppercase font-semibold text-ston-800 inline-block tracking-wide rounded-full border-2 hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focuse:ring-offset-2 disabled:cursor-not-allowed py-3 px-4 md:px-6 sm:py-4",
+    secondary: "uppercase font-semibold text-ston-800 inline-block tracking-wide rounded-full border-2 hover:bg-stone-800 hover:text-stone-800 transition-colors duration-300 focus:bg-stone-300 focus:hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focuse:ring-offset-2 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 sm:py-3.5",
   };
 
   if(to) return <Link to={to} className={styles[type]}>{children}</Link>
