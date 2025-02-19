0dc63691438a929a14295862ffc671ea4441fd4e
Federico-I <threxos.48@gmail.com>
2025-02-19 18:56:02 +0000

    Clearing-Uasername-cart-Delete Item

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -4,20 +4,6 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
-function CartOverview() {
-
-  return (
-    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 uppercase text-stone-200 text-sm sm:px-6 md:text-base">
-      <p className="font-semibold space-x-4 text-stone-300 sm:space-x-6">
-        <span> pizzas</span>
-        <span>pp</span>
-      </p>
-      <Link href="/cart">Open cart &rarr;</Link>
-    </div>
-  );
-}
-
-export default CartOverview;
 
 
 /*
