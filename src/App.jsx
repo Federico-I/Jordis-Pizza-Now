9ab474f90598cc919f63533d4834f572c330fa00
Federico-I <threxos.48@gmail.com>
2025-02-20 23:54:54 +0000

    resetApp

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,3 @@ function App() {
 }
 
 export default App;
-
-//////////////////////////////////////
-//          Router - Data
-//////////////////////////////////////
-
-// After createRouter6.4 If you want to use new API's for data loaders, Data Fetchers or Data Actions, it's mandatory to use the CreateBrouserRouter structure (syntax) shown below. 
-
-
-/* 
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      { path:"/", element: <Home />},
-      { path:"/menu", element: <Menu />, loader: menuLoader, errorElement: <Error />},
-      { path:"/cart", element: <Cart />},
-      { path:"/order/new", element: <CreateOrder />, action: actionCreateOrder},
-      { path:"/order/:orderId", element: <Order />, loader: orderLoader, errorElement: <Error />},
-    ],
-  },
-]);
-
-function App() {
-
-  return <RouterProvider router={router} />;  
-
-}
-
-export default App;
-
-*/
