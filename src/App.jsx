8c7142542702795ed090e26f71e8c7f8d1b823ff
Federico-I <threxos.48@gmail.com>
2025-02-20 23:53:18 +0000

    reSettup

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./ui/Home";
-import Error from "./ui/Error";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import CreateOrder, { action as actionCreateOrder } from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
-
-const router = createBrowserRouter([
-  { 
-    element: <AppLayout />,
-    errorElement: <Error />,
-
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader, errorElement: <Error /> },
-      { path: "/cart", element: <Cart />},
-      { path: "/order/new", element: <CreateOrder />, action: actionCreateOrder },
-      { path: "/order/:order", element: <Order />, loader: orderLoader}
-    ]
-  },
-]);
 
 function App() {
-  return <RouterProvider router={router}/>;
+  const x = 23;
+
+  return <div>Hello{x}</div>;
 }
 
 export default App;
