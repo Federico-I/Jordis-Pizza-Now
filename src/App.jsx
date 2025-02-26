ed9829c8fe152fb0f676d5331fcb8418a3bf31d7
Federico-I <threxos.48@gmail.com>
2025-02-26 23:38:55 +0000

    settingNew-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/menu",
+    element: <Menu />
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />
+  },
+  {
+    path: "/order",
+    element: <Order />,
+  }
+])
 
 function App() {
   const x = 23;
