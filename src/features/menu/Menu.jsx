9784d6d753ec29f52bb9a1fa3dc452e34198c954
Federico-I <threxos.48@gmail.com>
2025-02-28 01:13:38 +0000

    setup-menu-loaders

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,7 +1,14 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { getMenu } from "../../services/apiRestaurant";
 
 function Menu() {
   return <h1>Menu</h1>;
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
