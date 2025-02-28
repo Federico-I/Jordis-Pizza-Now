a3d5f81023451219bafba08ffb62f49adf2cbdb6
Federico-I <threxos.48@gmail.com>
2025-02-28 02:01:08 +0000

    renderingMenuItem-inMenuComp

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -2,12 +2,15 @@
 import React from "react";
 import { getMenu } from "../../services/apiRestaurant";
 import { useLoaderData } from "react-router-dom";
+import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
   console.log(menu);
 
-  return <ul></ul>;
+  return <ul>
+    {menu.map(pizza => <MenuItem pizza={pizza} key={pizza.id}/>)}
+  </ul>;
 }
 
 export async function loader() {
