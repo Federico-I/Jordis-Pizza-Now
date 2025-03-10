ceddb7c7d3161ef00a08bd58bf485c1d1678a825
Federico-I <threxos.48@gmail.com>
2025-03-10 23:42:04 +0000

    userNamefile

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function UserName() {
+  return (
+    <div className='text-sm font-semibold'></div>
+  )
+}
+
+export default UserName;
