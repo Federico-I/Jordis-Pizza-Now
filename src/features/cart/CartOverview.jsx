777f4e6e10f7a8649728d4c371a3f7f92fc65b94
Federico-I <threxos.48@gmail.com>
2025-03-12 22:50:14 +0000

    overV-flex

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-600 text-sm text-stone-200 uppercase px-4 py3 sm:px-6">
+    <div className=" flex bg-stone-600 text-sm text-stone-200 uppercase px-4 py3 sm:px-6 md:text-base">
       <p className="font-semibold text-stone-400 space-x-4 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
