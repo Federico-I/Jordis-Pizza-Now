34626fade7fbc1cc52f133762940e9891eba10f7
Federico-I <threxos.48@gmail.com>
2025-03-12 22:46:38 +0000

    createUser-mediaQ-text

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-600 text-stone-200 uppercase px-4 py3 sm:px-6">
-      <p className="font-semibold text-stone-400 space-x-4">
+    <div className="bg-stone-600 text-sm text-stone-200 uppercase px-4 py3 sm:px-6">
+      <p className="font-semibold text-stone-400 space-x-4 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
