f650ec21be932875c157532a59b5efdef166f04e
Federico-I <threxos.48@gmail.com>
2025-03-19 20:16:01 +0000

    stylingInput-Search

--- src/features/order/SearchOrd.jsx
+++ src/features/order/SearchOrd.jsx
@@ -15,7 +15,7 @@ function SearchOrd() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <inpput placeholder="Enter Order Number" value={query} onChange={(e) => setQuery(e.target.value)}/>
+      <inpput placeholder="Enter Order Number" value={query} onChange={(e) => setQuery(e.target.value)} className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm placeholder:text-stone-400"/>
     </form>
   )
 }
