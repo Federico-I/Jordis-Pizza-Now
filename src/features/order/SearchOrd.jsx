da4e531329b008cac84c8c8295402ef2c30c6db3
Federico-I <threxos.48@gmail.com>
2025-03-19 20:27:20 +0000

    inputButton-transitionSearch

--- src/features/order/SearchOrd.jsx
+++ src/features/order/SearchOrd.jsx
@@ -15,7 +15,7 @@ function SearchOrd() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <inpput placeholder="Enter Order Number" value={query} onChange={(e) => setQuery(e.target.value)} className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm placeholder:text-stone-400"/>
+      <inpput placeholder="Enter Order Number" value={query} onChange={(e) => setQuery(e.target.value)} className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:w-72 sm:w-64 focus:outline-none focus:ring focus:ring-yellow-500"/>
     </form>
   )
 }
