7e3e5916f9f121d557e971ee35d5e8736bfa31e2
Federico-I <threxos.48@gmail.com>
2025-03-21 03:15:09 +0000

    LinkedButton-if

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,16 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { useNavigate, useRouteError } from 'react-router-dom';
+import { useRouteError } from 'react-router-dom';
+import LinkButton from './LinkButton';
 
 function Error() {
-  const navigate = useNavigate();
+
   const error = useRouteError();
 
   return (
     <div>
       <h1>Oops! There has been an error 😢</h1>
       <p>{error.data || error.message}</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+      <LinkButton>&larr; Go back</LinkButton>
     </div>
   );
 }
--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,10 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 
 function LinkButton({ children, to }) {
+
+  const navigate = useNavigate();
+  const className = 'text-sm text-blue-500 hover:text-blue-600 hover:underline';
+
+  if (to === "-1") return <button onClick={() => navigate(-1)}></button>
+
   return (
-    <Link to={to} className='text-sm text-blue-500 hover:text-blue-600 hover:underline'>{children}</Link>
+    <Link to={to} className={className}>{children}</Link>
   )
 }
 
