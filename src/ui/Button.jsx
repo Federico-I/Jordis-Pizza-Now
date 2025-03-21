d24f43fef30e5895a0e3b8a3792f2a984c922cbd
Federico-I <threxos.48@gmail.com>
2025-03-21 02:25:21 +0000

    adding-newLinkButton

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Button({ children, disabled }) {
   return (
-    <button disabled={disabled} className="bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed">
+    <button disabled={disabled} className="bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4">
       {children}
     </button>
   )
