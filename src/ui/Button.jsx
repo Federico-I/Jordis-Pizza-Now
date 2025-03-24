316c84eb23d86a161c4500ffb29765d5fc2d564f
Federico-I <threxos.48@gmail.com>
2025-03-24 22:11:21 +0000

    button-link-if

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to}) {
+
+  const className = "bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
   
-  if(to) return <Link>{children}</Link>
+  if(to) return <Link to={to} className={className}>{children}</Link>
 
   return (
-    <button disabled={disabled} className="bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4">
+    <button disabled={disabled} className={className}>
       {children}
     </button>
   )
