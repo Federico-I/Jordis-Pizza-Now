d5a0d3fba5cceca4b37eebc1624451e08a6647e2
Federico-I <threxos.48@gmail.com>
2025-03-25 23:31:55 +0000

    menuitem-styling-name-conditionalStyling

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -8,7 +8,7 @@ function Menu() {
   const menu = useLoaderData();
   console.log(menu);
 
-  return <ul className="divide divide-stone-300">
+  return <ul className="divide divide-stone-300 px-2">
     {menu.map((pizza) => <MenuItem pizza={pizza} key={pizza.id}/>)}
   </ul>;
 }
