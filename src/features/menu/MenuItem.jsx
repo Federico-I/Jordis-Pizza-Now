56877c1d4939bae0ea88fb3fc7c6119a54353b22
Federico-I <threxos.48@gmail.com>
2025-03-25 23:50:21 +0000

    menuItem-button-style-addCart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -2,6 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { formatCurrency } from "../../utils/helpers";
+import Button from "../../ui/Button";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
@@ -12,8 +13,9 @@ function MenuItem({ pizza }) {
       <div className="flex flex-col">
         <p className="font-medium">{name}</p>
         <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(', ')}</p>
-        <div className="mt-auto">
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-stone-600">Sold out</p>}
+          <Button>Add to Cart</Button>
         </div>
       </div>
     </li>
