a804baa80dbcdbc484690179a60796731a746ee4
Federico-I <threxos.48@gmail.com>
2025-03-26 02:29:28 +0000

    newButtonstyles

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,9 +3,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to}) {
+function Button({ children, disabled, to, type}) {
 
   const className = "bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+
+  const styles = "bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
   
   if(to) return <Link to={to} className={className}>{children}</Link>
 
