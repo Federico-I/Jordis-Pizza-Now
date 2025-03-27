34399ca9e53e5fcb598092fb0276c3b51752b5de
Federico-I <threxos.48@gmail.com>
2025-03-27 23:45:09 +0000

    newStyles-classification-Button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,7 +7,9 @@ function Button({ children, disabled, to, type}) {
 
   const className = "bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
 
-  const styles = "bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+  const styles = { base: "bg-yellow-400 font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed",
+    primary: "",
+  };
   
   if(to) return <Link to={to} className={className}>{children}</Link>
 
