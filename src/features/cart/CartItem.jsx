0a68d9c918ee1e78077394a5c33c93f3beb6ab90
Federico-I <threxos.48@gmail.com>
2025-03-31 22:51:25 +0100

    cartItem-ButtonStyling

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/prop-types */
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
+    <li className="py-3">
       <p>
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center justify-between">
+        <p className="text-sm front-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
