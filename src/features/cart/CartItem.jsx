facb56a59ef40310bb6cd042a0141b8670609803
Federico-I <threxos.48@gmail.com>
2025-03-31 23:02:06 +0100

    mediaQ-cartItem

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -6,11 +6,11 @@ function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li className="py-3">
-      <p>
+    <li className="py-3 sm:flex sm:item-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm front-bold">{formatCurrency(totalPrice)}</p>
         <Button type="small">Delete</Button>
       </div>
