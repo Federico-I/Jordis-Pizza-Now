3c170c3b29e3b36827caab5177154952e498ad0b
Federico-I <threxos.48@gmail.com>
2025-04-02 02:50:00 +0100

    Cr-Orderstyled-webIcon

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -12,7 +12,7 @@ function Button({ children, disabled, to, type}) {
   const styles = { 
     primary: baseStyle + "px-4 py-3 sm:px-6 sm:py-4",
     samll: baseStyle + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
-    secondary: "font-semibold uppercase border-2 border-stone-300 text-stone-800 tracking-wide hover:bg-stone-300 transition-colors duration-400 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed md:py-3.5",
+    secondary: "inilne-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
   
   if(to) return <Link to={to} className={className}>{children}</Link>
