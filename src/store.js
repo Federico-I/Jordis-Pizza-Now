ee9bedb2aa2bcc343dac80a2dd2b90391e7519e8
Federico-I <threxos.48@gmail.com>
2025-04-07 23:32:44 +0100

    setting-store-usereducer

---
+++ src/store.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React, { useReducer } from "react";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({ 
+  reducer: {
+    user: useReducer,
+    },
+ });
+
+ export default store;
