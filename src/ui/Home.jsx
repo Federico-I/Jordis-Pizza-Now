120269cd4cb888f2f788eafd2492fb4d57cfe9f3
Federico-I <threxos.48@gmail.com>
2025-04-09 22:35:26 +0100

    userName-button-Home

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,8 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import CreateUser from "../features/user/CreateUser";
+import { useSelector } from "react-redux";
+import Button from "./Button";
 
 function Home() {
+  const username = useSelector((state) => state.user.username)
+
   return (
     <div className="my-10 text-center sm:my-16 px-4">
       <h1 className="text-xl mb-8 font-semibold text-center md:text-3xl">
@@ -13,7 +17,7 @@ function Home() {
         </span>
       </h1>
 
-      <CreateUser />
+      {username === "" ? <CreateUser /> : <Button to="/menu" type="primary"></Button>}
     </div>
   );
 }
