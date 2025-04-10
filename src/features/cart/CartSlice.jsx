0276219cea0aa9f492aed5d73a6efb627d77b56e
Federico-I <threxos.48@gmail.com>
2025-04-10 23:58:13 +0100

    cartSlice-adding-reducer-addItem-deleteItem

--- src/features/cart/CartSlice.jsx
+++ src/features/cart/CartSlice.jsx
@@ -3,15 +3,31 @@ import { createSlice } from '@reduxjs/toolkit';
 
 
 const initialState = {
-  cart: [],
+  // cart: [],
+
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      qunatity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
 };
 
 const cartSlice = createSlice({ 
   name: "cart",
   initialState,
   reducer: {
-    addItem(state, action) {},
-    deleteItem(state, action) {},
+    addItem(state, action) {
+      // payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
     increaseItemQuantity(state, action) {},
     decreaseItemQuantity(state, action) {},
     clearCart(state, action) {},
