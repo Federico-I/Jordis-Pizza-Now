968e1bb86c8cc60fcab7bd7e1bc077f488690dea
Federico-I <threxos.48@gmail.com>
2025-04-10 23:42:00 +0100

    carSlice-initialState

---
+++ src/features/cart/CartSlice.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({ 
+  name: "cart",
+  initialState,
+  reducer: {
+    addItem(state, action) {},
+    deleteItem(state, action) {},
+    increaseItemQuantity(state, action) {},
+    decreaseItemQuantity(state, action) {},
+    clearCart(state, action) {},
+  },
+});
