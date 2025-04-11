80fd51ba868f1d4412e9b2f1bb4608224622d5ed
Federico-I <threxos.48@gmail.com>
2025-04-11 05:00:34 +0100

    decraseItem-totPrice-CartSlice

--- src/features/cart/CartSlice.jsx
+++ src/features/cart/CartSlice.jsx
@@ -35,7 +35,13 @@ const cartSlice = createSlice({
       item.quantity++;
       item.totalPrice = item.quantity * item.unitPrice;
     },
-    decreaseItemQuantity(state, action) {},
+    decreaseItemQuantity(state, action) {
+      // payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
     clearCart(state, action) {},
   },
 });
