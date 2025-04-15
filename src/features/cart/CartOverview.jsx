aaba1fd15e61a3f659453830f29df1eb703804a4
Federico-I <threxos.48@gmail.com>
2025-04-15 01:51:35 +0100

    CartOverview-setCartQuantity-useSelector

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,12 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function CartOverview() {
+  const CartQuantity = useSelector((state) => state.cart.cart.reduce((sum, item) => sum + item.quantity, 0)
+  );
+
   return (
     <div className=" flex items-center justify-between bg-stone-600 text-sm text-stone-200 uppercase px-4 py3 sm:px-6 md:text-base">
       <p className="font-semibold text-stone-400 space-x-4 sm:space-x-6">
-        <span>23 pizzas</span>
+        <span>{CartQuantity} pizzas</span>
         <span>$23.45</span>
       </p>
       <Link href="/cart">Open cart &rarr;</Link>
