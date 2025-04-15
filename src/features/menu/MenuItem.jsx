be8e722f6684b0a6d8d1cb6be2140ccbbe455dc8
Federico-I <threxos.48@gmail.com>
2025-04-15 01:30:57 +0100

    implementing-handleAddToCart-MenuItem

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -5,7 +5,20 @@ import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
 
 function MenuItem({ pizza }) {
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart () {
+    const newItem = [
+      {
+        pizzaId: id,
+        name,
+        quantity: 1,
+        unitPrice,
+        totalPrice: unitPrice * 1,
+      },
+    ]
+  };
 
   return (
     <li className="flex gap-4 py-2">
@@ -15,7 +28,7 @@ function MenuItem({ pizza }) {
         <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(', ')}</p>
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-stone-600">Sold out</p>}
-          {!soldOut && <Button type="samll" onClick={}>Add to Cart</Button>}
+          {!soldOut && <Button type="samll" onClick={handleAddToCart}>Add to Cart</Button>}
         </div>
       </div>
     </li>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to, type}) {
+function Button({ children, disabled, to, type, onClick}) {
 
   const className = "bg-yellow-400 px-4 py-3 text-sm font-semibold uppercase text-stone-800 tracking-wide hover:bg-yellow-300 transition-colors duration-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
 
@@ -15,13 +15,21 @@ function Button({ children, disabled, to, type}) {
     secondary: "inilne-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
   
-  if(to) return <Link to={to} className={className}>{children}</Link>
+  if (to) return (
+    <Link to={to} className={styles[type]}>{children}</Link>
+  );
+
+  if (onClick) return (
+    <button onClick={onClick} disabled={disabled} className={className}>
+      {children}
+    </button>
+  );
 
   return (
     <button disabled={disabled} className={className}>
       {children}
     </button>
-  )
-}
+  );
+};
 
 export default Button;
