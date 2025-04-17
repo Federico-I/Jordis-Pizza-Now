9e76df14e42f5723e5271436a0f6680da6c6c3b2
Federico-I <threxos.48@gmail.com>
2025-04-18 00:11:03 +0100

    setting-deleteItem

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Button from '../../ui/Button';
+
+function DeleteItem() {
+  return (
+    <Button type="small">Delete</Button>
+  )
+};
+
+export default DeleteItem;
