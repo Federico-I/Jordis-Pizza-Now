5a6a075770216b713b3da3356f962d0971bfc60e
Federico-I <threxos.48@gmail.com>
2025-04-23 23:41:43 +0100

    udpateItemQ-File

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+function UpdateItemQuantity() {
+  return (
+    <div>UpdateItemQuantity</div>
+  )
+};
+
+export default UpdateItemQuantity;
