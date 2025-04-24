b099b8782109dff70e123543e04ef6e93012339a
Federico-I <threxos.48@gmail.com>
2025-04-24 23:50:59 +0100

    itemDecrease-Increase

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Button from '../../ui/Button';
+import { decreaseItemQuantity, increaseItemQuantity } from './CartSlice';
 
 function UpdateItemQuantity({ pizzaId }) {
   const dispatch = useDispatch();
 
   return (
    <div className='flex items-center gap-1 md:gap-3'>
-     <Button type="round" onClick={() => dispatch()}> - </Button>
-     <Button type="round"> + </Button>
+     <Button type="round" onClick={() => dispatch(decreaseItemQuantity(pizzaId))}> - </Button>
+     <Button type="round" onClick={() => dispatch(increaseItemQuantity(pizzaId))}> + </Button>
    </div>
   );
 }
