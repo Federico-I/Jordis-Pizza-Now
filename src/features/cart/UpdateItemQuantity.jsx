2da67accf3f4a2794e1724996c53c6f39a47a294
Federico-I <threxos.48@gmail.com>
2025-04-24 23:46:45 +0100

    UpdateItemQuantity-dispatch+

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import Button from '../../ui/Button';
 
 function UpdateItemQuantity({ pizzaId }) {
+  const dispatch = useDispatch();
+
   return (
    <div className='flex items-center gap-1 md:gap-3'>
-     <Button type="round">-</Button>
-     <Button type="round">+</Button>
+     <Button type="round" onClick={() => dispatch()}> - </Button>
+     <Button type="round"> + </Button>
    </div>
   );
 }
