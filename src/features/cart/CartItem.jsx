40c0d25a666f1ce658b21dfceadb200587e6c8ab
Federico-I <threxos.48@gmail.com>
2025-04-25 00:36:13 +0100

    quantity-span-updateItemQuantity

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/prop-types */
+import { useSelector } from "react-redux";
 import { formatCurrency } from "../../utils/helpers";
+import { getQuantityById } from "./CartSlice";
 import DeleteItem from "./DeleteItem";
 import UpdateItemQuantity from "./UpdateItemQuantity";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+
+  const currentQuanity = useSelector(getQuantityById(pizzaId));
 
   return (
     <li className="py-3 sm:flex sm:item-center sm:justify-between">
@@ -14,8 +18,8 @@ function CartItem({ item }) {
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm front-bold">{formatCurrency(totalPrice)}</p>
 
-        <UpdateItemQuantity pizzaId={pizzaId}/>
-        <DeleteItem pizzaId={pizzaId}/>
+        <UpdateItemQuantity pizzaId={ pizzaId } currentQuanity={currentQuanity}/>
+        <DeleteItem pizzaId={ pizzaId }/>
       </div>
     </li>
   );
--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,15 +1,17 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Button from '../../ui/Button';
 import { decreaseItemQuantity, increaseItemQuantity } from './CartSlice';
 import { useDispatch } from 'react-redux';
 
-function UpdateItemQuantity({ pizzaId }) {
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
   const dispatch = useDispatch();
 
   return (
    <div className='flex items-center gap-1 md:gap-3'>
      <Button type="round" onClick={() => dispatch(decreaseItemQuantity(pizzaId))}> - </Button>
+     <span>{currentQuantity}</span>
      <Button type="round" onClick={() => dispatch(increaseItemQuantity(pizzaId))}> + </Button>
    </div>
   );
