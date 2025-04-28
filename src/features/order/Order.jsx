a139a1e0919fbbdfdd2526a3c06dc14637759954
Federico-I <threxos.48@gmail.com>
2025-04-28 23:42:26 +0100

    fixing-pizzaId-OrderItem-Props

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -8,6 +8,7 @@ import {
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import OrderItem from "./OrderItem";
 
 
 function Order() {
@@ -47,7 +48,9 @@ function Order() {
       </div>
       
       <ul className="dive-stone-200 divide-y border-b border-t">
-        {cart.map((item) => <OrderItem/> )}
+        {cart.map((item) => ( 
+          <OrderItem item={item} key={item.pizzaId} /> 
+          ))}
       </ul>
 
       <div className="space-y-2 bg-stone-200 px-6 py-5">
