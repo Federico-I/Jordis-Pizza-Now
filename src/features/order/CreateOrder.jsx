f4fef22920758cdfacc6336cb1ff12232ef5de53
Federico-I <threxos.48@gmail.com>
2025-05-07 23:59:20 +0100

    styling-button-position

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -62,13 +62,13 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex gap-2 flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex gap-2 flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input className="input w-full" type="text" name="address" required />
           </div>
 
-          <span className="absolute">
+          <span className="absolute right-[3px] z-50">
             <Button type="small" onClick={() => dispatch(fetchAddress())}>Get Position</Button>
           </span>
         </div>
