e0bea77807da9d703ea2e115b53285c56cbf2300
Federico-I <threxos.48@gmail.com>
2025-05-20 23:28:17 +0100

    funtion-action-async

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Button from '../../ui/Button';
 import { useFetcher } from 'react-router-dom';
@@ -6,10 +7,14 @@ function UpdateOrder({ order }) {
   const fetcher = useFetcher();
 
   return (
-    <fetcher.Form>
+    <fetcher.Form method='PATCH' className='text-right'>
       <Button type="primary">Priority</Button>
     </fetcher.Form>
   )
 };
 
 export default UpdateOrder;
+
+async function action({ request, params }) {
+  return null;
+}
