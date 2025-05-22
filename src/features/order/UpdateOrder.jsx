38bf80650602f8599546eb78341ffb3a3e05e4a8
Federico-I <threxos.48@gmail.com>
2025-05-22 15:52:10 +0100

    await-params-orderId

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Button from '../../ui/Button';
 import { useFetcher } from 'react-router-dom';
+import { updateOrder } from '../../services/apiRestaurant';
 
 function UpdateOrder({ order }) {
   const fetcher = useFetcher();
@@ -17,5 +18,6 @@ export default UpdateOrder;
 
 async function action({ request, params }) {
   const data = { priority: true };
+  await updateOrder(params.orderId, data);
   return null;
 }
